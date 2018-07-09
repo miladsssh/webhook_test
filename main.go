025544a6d9d0aded9dbdfcde7fb152b9843b23f2
milad.miri <miladsssh@gmail.com>
2018-07-09 23:26:23 +0200

    printing env var2

--- main.go
+++ main.go
@@ -5,19 +5,17 @@ import (
 	"context"
 	"github.com/aws/aws-lambda-go/lambda"
 	"os"
-	"strings"
 	"log"
 )
 
 
 func HandleRequest(_ context.Context) (string, error) {
-	return fmt.Sprintf("Hello world from %s!", os.Getenv("MyEnvironment") ), nil
+	return fmt.Sprintf("Hello world from %s!", os.Getenv("Myenvironment") ), nil
 }
 
 func main() {
 	for _, e := range os.Environ() {
-		pair := strings.Split(e, "=")
-		log.Printf("Environmet variables: %s\n", pair[0])
+		log.Printf("Environmet variables: %s\n", e)
 	}
 	lambda.Start(HandleRequest)
 }
